ea1b63a25eea584da7a5adea774540eb0104877d
jaypetrov <jay.petrov89@gmail.com>
2023-01-20 00:36:27 -0500

    Second batch of files

---
+++ 02-backend/spring-boot-library/src/main/java/com/fullstackapp/springbootlibrary/SpringBootLibraryApplication.java
@@ -0,0 +1,15 @@
+package com.fullstackapp.springbootlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+public class SpringBootLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLibraryApplication.class, args);
+	}
+
+}
---
+++ 02-backend/spring-boot-library/src/main/java/com/fullstackapp/springbootlibrary/dao/BookRepository.java
@@ -0,0 +1,18 @@
+package com.fullstackapp.springbootlibrary.dao;
+
+import com.fullstackapp.springbootlibrary.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable Pageable);
+
+    Page<Book> findByCategory(@RequestParam("category") String category, Pageable Pageable);
+}
+
+
