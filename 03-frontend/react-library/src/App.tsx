7ebd89f6ed2e640bce710397b6a2cb4845abc6de
jaypetrov <jay.petrov89@gmail.com>
2023-01-20 22:37:10 -0500

    Adding Routing to the app part 1
    Adding Routing to the app part 1

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -5,19 +5,22 @@ import { Footer } from "./Layouts/NavbarAndFooter/Footer";
 import { Navbar } from "./Layouts/NavbarAndFooter/Navbar";
 import { SearchBook } from "./Layouts/SearchBooksPage/components/SearchBook";
 import { SearchBooksPage } from "./Layouts/SearchBooksPage/SearchBooksPage";
- 
-
-  
+import { Redirect, Route, Switch, useHistory } from "react-router-dom";
 
 export const App = () => {
   return (
     <>
-     <Navbar/>
-     {/* <HomePage/> */}
-     <SearchBooksPage/>
-     <Footer/>
-     </>
+      <Navbar />
+      <Switch>
+        <Route path="/"  >
+          <HomePage />
+        </Route>
+        <Route path="search">
+          <SearchBooksPage />
+        </Route>
+      </Switch>
+      <Footer />
+    </>
   );
-}
+};
 export default App;
-
