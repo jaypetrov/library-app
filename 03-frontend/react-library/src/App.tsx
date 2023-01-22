84a46303cb4e930dca56818130e01591f1abc77b
jaypetrov <jay.petrov89@gmail.com>
2023-01-22 15:30:36 -0500

    Routing part 2

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -10,20 +10,19 @@ import { Redirect, Route, Switch } from "react-router-dom";
 export const App = () => {
   return (
     <div className="d-flex flex-column min-vh-100">
-      
       <Navbar />
       <div className="flex-grow-1">
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to='/home'/>
-        </Route>
-        <Route path="/home">
-          <HomePage />
-        </Route>
-        <Route path="/search">
-          <SearchBooksPage />
-        </Route>
-      </Switch>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/home" />
+          </Route>
+          <Route path="/home">
+            <HomePage />
+          </Route>
+          <Route path="/search">
+            <SearchBooksPage />
+          </Route>
+        </Switch>
       </div>
       <Footer />
     </div>
